34b5526882656555ec5f1c0137ad3e81dd96b3d2
abudiar <alan@brik.id>
2024-08-23 18:16:34 +0700

    Fix Layer keys on KRSH V2

--- keyboards/krsh/preonic/keymaps/krsh_v2/keymap.c
+++ keyboards/krsh/preonic/keymaps/krsh_v2/keymap.c
@@ -14,7 +14,7 @@ enum custom_keycodes {
 #define _F_KEYS 2
 #define _NUMP 3
 #define _E 4
-#define _F 5
+#define _GAME 5
 #define _G 6
 #define _H 7
 #define _I 8
@@ -22,7 +22,7 @@ enum custom_keycodes {
 #define _K 10
 #define _L 11
 #define _M 12
-#define _N 13
+#define _SYS 13
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
@@ -31,7 +31,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_F_KEYS] = { ENCODER_CCW_CW(UG_VALD, UG_VALU) },
     [_NUMP] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
     [_E] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
-    [_F] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
+    [_GAME] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
     [_G] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
     [_H] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
     [_I] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
@@ -39,7 +39,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_K] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
     [_L] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
     [_M] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
-    [_N] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
+    [_SYS] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT) },
 };
 #endif
 
@@ -80,17 +80,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     // [_XTRA] = LAYOUT_preonic_grid(
     //     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,       _______,    _______,     _______,
-    //     TO(_N),   TO(_E),   TO(_F),   TO(_G),   _______,  KC_MUTE,  KC_MPLY,  _______,     _______,       KC_MS_UP,   _______,     KC_MS_WH_UP,
+    //     TO(_SYS),   TO(_E),   TO(_GAME),   TO(_G),   _______,  KC_MUTE,  KC_MPLY,  _______,     _______,       KC_MS_UP,   _______,     KC_MS_WH_UP,
     //     _______,  TO(_H),   TO(_I),   TO(_J),   _______,  KC_VOLU,  KC_MNXT,  _______,     KC_MS_LEFT,    KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN,
     //     _______,  TO(_K),   TO(_L),   TO(_M),   _______,  KC_VOLD,  KC_MPRV,  _______,     KC_MS_WH_LEFT, _______,    KC_PGUP,     KC_MS_WH_RIGHT,
     //     _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MS_BTN1,  KC_MS_BTN2,    KC_HOME,    KC_PGDN,     KC_END
     // ),
     [_XTRA] = LAYOUT_preonic_grid(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,        KC_MS_WH_UP,   _______,      _______,  _______,
-        TO(_N),   TO(_E),   TO(_F),   TO(_G),   _______,  KC_MUTE,  KC_MPLY,  KC_MS_WH_LEFT,  KC_MS_WH_DOWN, _______,      _______,  _______,
-        _______,  TO(_H),   TO(_I),   TO(_J),   _______,  KC_VOLU,  KC_MNXT,  KC_MS_BTN1,     KC_MS_UP,      KC_MS_BTN2,   _______,  _______,
-        _______,  TO(_K),   TO(_L),   TO(_M),   _______,  KC_VOLD,  KC_MPRV,  KC_MS_LEFT,     KC_MS_DOWN,    KC_MS_RIGHT,  KC_PGUP,  KC_MS_WH_RIGHT,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,        _______,       KC_HOME,      KC_PGDN,  KC_END
+        _______,      _______,  _______,   _______,  _______,  _______,  _______,  _______,        KC_MS_WH_UP,   _______,      _______,  _______,
+        TG(_SYS),     TG(_E),   TG(_GAME), TG(_G),   _______,  KC_MUTE,  KC_MPLY,  KC_MS_WH_LEFT,  KC_MS_WH_DOWN, _______,      _______,  _______,
+        _______,      TG(_H),   TG(_I),    TG(_J),   _______,  KC_VOLU,  KC_MNXT,  KC_MS_BTN1,     KC_MS_UP,      KC_MS_BTN2,   _______,  _______,
+        _______,      TG(_K),   TG(_L),    TG(_M),   _______,  KC_VOLD,  KC_MPRV,  KC_MS_LEFT,     KC_MS_DOWN,    KC_MS_RIGHT,  KC_PGUP,  KC_MS_WH_RIGHT,
+        TO(_QWERTY),  _______,  _______,   _______,  _______,  _______,  _______,  _______,        _______,       KC_HOME,      KC_PGDN,  KC_END
     ),
 
     /*  _F_KEYS
@@ -156,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 
-    /*  _F
+    /*  _GAME
      * -------------------------------------------------------------------------------------------------------------------------
      * |         |         |         |         |         |         |         |         |         |         |         |         |
      * -------------------------------------------------------------------------------------------------------------------------
@@ -169,11 +169,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |         |         |         |         |         |         |         |         |         |         |         |         |
      * -------------------------------------------------------------------------------------------------------------------------
      */
-    [_F] = LAYOUT_preonic_grid(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,       KC_I,     KC_O,     KC_P,     TO(_QWERTY),
-        KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,       KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,       KC_COMM,  KC_DOT,   KC_UP,    RSFT_T(KC_ENT),
+    [_GAME] = LAYOUT_preonic_grid(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,     _______,  _______,  _______,  _______,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,        KC_I,     KC_O,     KC_P,     TO(_QWERTY),
+        KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,        KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,        KC_COMM,  KC_DOT,   KC_UP,    RSFT_T(KC_ENT),
         KC_LCTL,  KC_LALT,  KC_LALT,  KC_SPC,   KC_SPC,   KC_SPC,   KC_SPC,   TT(_F_KEYS), KC_SLSH,  KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
 
@@ -324,7 +324,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 
-    /*  _N
+    /*  _SYS
      * -------------------------------------------------------------------------------------------------------------------------
      * |         |         |         |         |         |         |         |         |         |         |         |         |
      * -------------------------------------------------------------------------------------------------------------------------
@@ -337,7 +337,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |         |         |         |         |         |         |         |         |         |         |         |         |
      * -------------------------------------------------------------------------------------------------------------------------
      */
-    [_N] = LAYOUT_preonic_grid(
+    [_SYS] = LAYOUT_preonic_grid(
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
         // RESET,    DEBUG,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  TO(_QWERTY),
